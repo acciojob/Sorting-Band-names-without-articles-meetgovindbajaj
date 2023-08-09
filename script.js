157f2f8c8a58d72f847ca4c9e8c7d27fa14ac2e9
Govind Bajaj <meetgovindbajaj@gmail.com>
2023-08-09 23:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
 
+function dynamicSort(a, b) {
+  const regex = new RegExp("(^(an|An|a|A|the|The))", "i");
+  let one = regex.test(a) ? a.split(regex) : [a],
+    two = regex.test(b) ? b.split(regex) : [b];
+  one = one[one.length - 1].trim().toUpperCase();
+  two = two[two.length - 1].trim().toUpperCase();
+  return one < two ? -1 : one > two ? 1 : 0;
+}
+let touristSpots = ["The Virupaksha Temple", "Victoria Memorial", "Tajmahal"];
+touristSpots.sort(dynamicSort);
